158999fb8524bd15e8dbc20f87cb2eb1c7a91078
Todd Garrison <toddlgarrison@gmail.com>
2024-02-21 15:30:43 -0500

    Add buttons

--- src/index.tsx
+++ src/index.tsx
@@ -1,10 +1,29 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
-class App extends React.Component {
-    render() {
-        return <div>Hi There</div>
+interface AppProps {
+    color?: string;
+}
+
+class App extends React.Component<AppProps> {
+    state = { counter: 0 };
+
+    onIncrement = (): void => {
+        this.setState({ counter: this.state.counter + 1 });
     }
+
+    onDecrement = (): void => {
+        this.setState({ counter: this.state.counter - 1 });
+    }
+
+    render() {
+        return (
+        <div>
+            <button onClick={this.onIncrement}>Increment</button>
+            <button onClick={this.onDecrement}>Decrement</button>
+            {this.state.counter}
+        </div>
+    )}
 };
 
 ReactDOM.render(
